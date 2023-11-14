12b155b7b0bba6abe181e142b0fef798ff006feb
merryberry <stardust010110@gmail.com>
2023-11-14 23:40:12 +0900

    signal handler added

--- main.c
+++ main.c
@@ -6,10 +6,29 @@
 #include <input.h>
 #include <web_server.h>
 
+/* 여기서 sigchld 핸들러 구현 */
+static void
+sigchldHandler(int sig)
+{
+    /* 구현 */
+    printf("handler: Caught SIGCHLD : 17\n");
+    printf("handler: returning");
+}
+
 int main()
 {
     pid_t spid, gpid, ipid, wpid;
     int status, savedErrno;
+    int sigCnt;
+    sigset_t blockMask, emptyMask;
+    struct sigaction sa;
+
+    /* 여기서 SIGCHLD 시그널  등록 */
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = sigchldHandler;
+    if(sigaction(SIGCHLD, &sa, NULL) == -1)
+        fprintf(stderr, "main() SIGCHLD sigaction() failed");
 
     printf("메인 함수입니다.\n");
     printf("시스템 서버를 생성합니다.\n");
--- web_server/web_server.c
+++ web_server/web_server.c
@@ -10,9 +10,21 @@ int create_web_server()
     pid_t systemPid;
 
     printf("여기서 Web Server 프로세스를 생성합니다.\n");
-    
-    if(!(systemPid = fork()))
-        execl("/usr/local/bin/filebrowser", "filebrowser", "-p", "8282", (char *) NULL);
+
+    /* fork + exec 를 이용하세요 */
+    /* exec으로 google-chrome-stable을 실행 하세요. */
+
+    switch (systemPid = fork()) {
+    case -1:
+        printf("fork failed\n");
+    case 0:
+        if (execl("/usr/local/bin/filebrowser", "filebrowser", "-p", "8282", (char *) NULL)) {
+            printf("execfailed\n");
+        }
+        break;
+    default:
+        break;
+    }
 
     return 0;
 }
